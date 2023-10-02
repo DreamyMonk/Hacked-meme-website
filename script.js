9d5b1efd6f9589eaedd5689b8bdef517788b59ab
Saptrishi Das Biswas <138619155+DreamyMonk@users.noreply.github.com>
2023-10-02 22:45:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.onload = async () => {
     const video = document.getElementById("video");
+    const start = document.getElementById("start");  // Assuming this element exists
     const loading = document.getElementById("loading");
     const data = document.getElementById("data");
-    const start = document.getElementById("start");  // Reference to the button
-    let videoHasEnded = false;
 
     const error = (message) => {
         loading.innerText = message;
@@ -13,79 +12,63 @@ window.onload = async () => {
     const pick = (array) => array[Math.floor(Math.random() * array.length)];
     const hacked_statements = ["Yes", "Maybe", "Most Likely", "Highly Probable", "Potentially", "Unlikely But Still Possible", "Almost Certainly", "Definitely", "Absolutely"];
 
-    const openFullscreen = (elem) => {
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.mozRequestFullScreen) { /* Firefox */
-            elem.mozRequestFullScreen();
-        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { /* IE/Edge */
-            elem.msRequestFullscreen();
+    // Handle full screen changes
+    const handleFullScreenChange = (e) => {
+        if (
+            document.fullscreenElement || 
+            document.webkitFullscreenElement || 
+            document.msFullscreenElement
+        ) {
+            // Still in full screen, do nothing
+        } else {
+            // Attempt to go back to full screen
+            video.requestFullscreen();
         }
     };
 
-    document.addEventListener("keydown", function (e) {
-        if (e.key === "Escape" && !videoHasEnded) {
-            e.preventDefault();
-        }
-    });
+    document.addEventListener('fullscreenchange', handleFullScreenChange);
+    document.addEventListener('webkitfullscreenchange', handleFullScreenChange); // Safari
+    document.addEventListener('msfullscreenchange', handleFullScreenChange); // IE/Edge
 
     try {
         const memes = [];
         const push = (tit, con, add) => memes.push(`${tit}: ${con}${add || ""}`);
-
         let step = 0;
-        
-        data.style.fontSize = "16px"; // default font size for testing
-
-        let my_ip = await (await fetch("https://wtfismyip.com/json").catch()).json().catch();
-        let ip_data = await (await fetch(`https://uncors.vercel.app/?url=http://ip-api.com/json/${my_ip.YourFuckingIPAddress}`).catch()).json().catch();
-
-        const videoData = await fetch("./assets/video.mp4").catch(error);
-        video.src = URL.createObjectURL(await videoData.blob());
-        video.load();
-
-        video.oncanplaythrough = async () => {
-            loading.style.display = "none";
-            start.style.display = "flex";
-            // ... [rest of the oncanplaythrough function]
-        };
+        let fontSize = Math.min(window.innerHeight / 10, window.innerWidth / 10);
+        data.style.fontSize = `${fontSize}px`;
+        // ... (rest of your initialization code)
 
         start.onclick = async () => {
-            openFullscreen(document.documentElement);  // Enter fullscreen mode
-            videoHasEnded = false; // Reset the video end flag
-            start.style.display = "none";
-            video.style.display = "flex";
-            video.play();
+            if (video.requestFullscreen) {
+                video.requestFullscreen();
+            } else if (video.webkitRequestFullscreen) { // Safari
+                video.webkitRequestFullscreen();
+            } else if (video.msRequestFullscreen) { // IE/Edge
+                video.msRequestFullscreen();
+            }
 
-            const interval = setInterval(() => {
-                const time = video.currentTime - 2.1 - (step * 60) / 132; // 132 bpm moment
-                if (step >= memes.length) step = -Infinity;
-                if (step < 0) return clearInterval(interval);
-                if (time >= 0) {
-                    if (step == 0) document.title = `Hacked by Your Name [${my_ip ? my_ip.YourFuckingIPAddress : "::ffff:172.70.126.134"}]`;
-                    const el = document.createElement("span");
-                    el.textContent = `${memes[step]}`;
-                    step++;
-                    data.appendChild(el);
-                    const height = data.getBoundingClientRect().height;
-                    if (height >= window.innerHeight) {
-                        let fontSize = parseFloat(getComputedStyle(data).fontSize);
-                        fontSize *= 0.88;
-                        data.style.fontSize = `${fontSize}px`;
-                    }
-                }
-            }, 5);
+            // ... (rest of your start.onclick function)
         };
 
         video.onended = () => {
-            videoHasEnded = true; // Set the video end flag
+            // Remove the event listeners, allowing the user to exit fullscreen
+            document.removeEventListener('fullscreenchange', handleFullScreenChange);
+            document.removeEventListener('webkitfullscreenchange', handleFullScreenChange);
+            document.removeEventListener('msfullscreenchange', handleFullScreenChange);
+
+            // Exit fullscreen
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) { // Safari
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { // IE/Edge
+                document.msExitFullscreen();
+            }
+
             video.style.display = "none";
             step = -Infinity;
-            loading.innerText = "Press Escape to exit full screen.";
-            loading.style.display = "block";
         };
+
     } catch (e) {
         error(`${e.message}`);
     }
